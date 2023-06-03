26d11cc7a456d831c3cba1dd7d2439f37c7037ca
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-06-04 00:37:04 +0300

    1 file modified

--- src/app/view-tasks/view-tasks.component.ts
+++ src/app/view-tasks/view-tasks.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { Todo, TodoService } from './../service/task.service';
